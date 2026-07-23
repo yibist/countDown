063cd3df7d7fce78f922b520de07b2424c6ddea7
yibist <yibist@gmail.com>
2026-07-23 10:18:22 +0200

    adds player

---
+++ scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+const ACCELERATION = 20.0  
+const FRICTION = 8.0
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("moveLeft", "moveRight")
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION)
+	elif is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, FRICTION)
+
+	move_and_slide()
