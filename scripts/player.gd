987117f97dfe81209b4a49625d0f492c11c8314d
yibist <yibist@gmail.com>
2026-07-24 19:37:58 +0200

    changes

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,16 +2,18 @@ extends CharacterBody2D
 
 @export var grapleHead: CharacterBody2D
 @export var landParticles: CPUParticles2D
+@export var sprite: Sprite2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const ACCELERATION = 20.0  
 const FRICTION = 8.0
 const SWINGFORCE = 300
 var jumps = 2;
+const WHEELRADIUS = 30
+
 
 
 func _physics_process(delta: float) -> void:
-	
 	var hookPoint = grapleHead.global_position
 	var maxLength = grapleHead.ropeLength
 	var toPlayer = global_position - hookPoint
@@ -20,7 +22,7 @@ func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if is_on_floor():
+	else:
 		jumps = 2
 
 	if Input.is_action_just_pressed("jump") && jumps > 0:
@@ -60,4 +62,58 @@ func _physics_process(delta: float) -> void:
 		var mousePos = get_global_mouse_position()
 		var mouseDir = (mousePos - global_position).normalized()
 		velocity = mouseDir * (max(currentSpeed,SPEED) + 100)
+		
+		
+	
+	var tempVelocity = velocity
 	move_and_slide()
+	emit_land_particles(tempVelocity)
+	update_wheel_rotation(delta)
+	
+	
+	
+func emit_land_particles(currentVelocity:Vector2):
+	var collisionCount = get_slide_collision_count()
+	if collisionCount == 0:
+		return
+
+	
+	for i in range(collisionCount):
+		var collision = get_slide_collision(i)
+		if collision == null:
+			continue
+		
+		var collisionNormal = collision.get_normal()
+		var collisionPoint = collision.get_position()
+		
+		var impactSpeed = currentVelocity.dot(-collisionNormal)
+		
+		if impactSpeed > 500:
+			spawn_particles(collisionPoint, collisionNormal, impactSpeed)
+
+func spawn_particles(position: Vector2, normal: Vector2, impactSpeed: float = 0.0):
+	if landParticles == null:
+		return
+	
+	landParticles.global_position = position
+	landParticles.rotation = normal.angle() + PI/2
+	
+	var speedFactor = max(0, impactSpeed - 500.0) / 100.0
+	landParticles.scale = Vector2.ONE * (1.0 + speedFactor * 0.5)
+	landParticles.amount = int(10 + pow(speedFactor, 2) * 10)
+	landParticles.restart()
+	landParticles.emitting = true
+
+func update_wheel_rotation(delta:float):
+
+	var angularVelocity = velocity.x / WHEELRADIUS
+	var speed_factor = 1.0
+	angularVelocity *= speed_factor
+	sprite.rotation += angularVelocity * delta
+	
+	
+		
+		
+		
+	
+		
