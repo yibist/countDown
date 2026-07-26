0fce193cdc62e2778b09a4d972f18dd8e15b70fe
Klemen-Z <klemenzmahar@gmail.com>
2026-07-26 16:24:45 +0200

    removed debug print in global event bus

--- scripts/globalEventBus.gd
+++ scripts/globalEventBus.gd
@@ -2,9 +2,3 @@ extends Node
 
 signal playerDeath
 signal playerWin
-
-func _ready() -> void:
-	GlobalEventBus.connect("playerDeath", _on_playerDeath)
-
-func _on_playerDeath() -> void:
-	print("Player died")
